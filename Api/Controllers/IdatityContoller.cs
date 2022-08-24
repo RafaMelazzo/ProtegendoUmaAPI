4bb524efc95cf6122e5bea3876a765d8522ce5d7
Pichau <rafamelazzo@gmail.com>
2022-08-24 14:55:21 -0300

    Add project files.

---
+++ Api/Controllers/IdatityContoller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Api.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdatityContoller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult
+                (
+                    from c in User.Claims
+                    select new { c.Type, c.Value }
+                );
+        }
+    }
+}
